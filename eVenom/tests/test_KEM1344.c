fea39cbf137a0cc2b5130203d2f93c91d1412374
Make <71177618+Make1205@users.noreply.github.com>
2026-04-20 12:55:46 +0800

    Rename Frodo/eFrodo paths to Venom naming

---
+++ eVenom/tests/test_KEM1344.c
@@ -0,0 +1,21 @@
+/********************************************************************************************
+* MAMBA-Venom: Learning with Errors Key Encapsulation
+*
+* Abstract: setting parameters to test eMAMBA-Venom-1344
+*********************************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "ds_benchmark.h"
+#include "../src/api_venom5.h"
+
+
+#define SYSTEM_NAME    "eMAMBA-Venom-1344"
+
+#define crypto_kem_keypair_enc        crypto_kem_keypair_enc_eVenom1344
+#define crypto_kem_dec                crypto_kem_dec_eVenom1344
+#define shake                         shake256
+
+#include "test_kem.c"
